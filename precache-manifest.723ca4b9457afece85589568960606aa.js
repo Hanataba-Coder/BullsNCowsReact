59981f68df87a79d9545ad7b8187811c39f32949
Yodsapat <yodsapat.p@giztix.com>
2020-09-30 21:38:32 +0700

    Updates

---
+++ precache-manifest.723ca4b9457afece85589568960606aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a5389a0766c74235fcbf1df9d531a8",
+    "url": "/BullsNCowsReact/index.html"
+  },
+  {
+    "revision": "351bfc11a63c407e2f53",
+    "url": "/BullsNCowsReact/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "d082dcfe7d4bcd584b67",
+    "url": "/BullsNCowsReact/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "351bfc11a63c407e2f53",
+    "url": "/BullsNCowsReact/static/js/2.fa7781bb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/BullsNCowsReact/static/js/2.fa7781bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d082dcfe7d4bcd584b67",
+    "url": "/BullsNCowsReact/static/js/main.b9fbf575.chunk.js"
+  },
+  {
+    "revision": "1d4c9aa664200421ff83",
+    "url": "/BullsNCowsReact/static/js/runtime-main.3a425501.js"
+  }
+]);
